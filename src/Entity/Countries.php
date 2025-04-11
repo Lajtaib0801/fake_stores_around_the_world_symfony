c5873acef0e71ec4d215e1316d69015dbf6aab9e
Lajtaib0801 <lajtaibenja@gmail.com>
2025-04-11 23:12:53 +0200

    first commit with Cities and Countries entities including the corresponding autogenerated files

---
+++ src/Entity/Countries.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CountriesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CountriesRepository::class)]
+class Countries
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, unique: true)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, unique: true)]
+    private ?string $code = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $continent = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): static
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getContinent(): ?string
+    {
+        return $this->continent;
+    }
+
+    public function setContinent(string $continent): static
+    {
+        $this->continent = $continent;
+
+        return $this;
+    }
+}
