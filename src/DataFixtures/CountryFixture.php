ea2102012764138fb2a9213dedbfcca331fa3294
Lajtaib0801 <lajtaibenja@gmail.com>
2025-04-12 22:45:55 +0200

    country fixture now complete

--- src/DataFixtures/CountryFixture.php
+++ src/DataFixtures/CountryFixture.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Country;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,6 +10,16 @@ class CountryFixture extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        
+        $countries = json_decode(file_get_contents(__DIR__.'\..\..\assets\data\countries.json'), true);
+
+        foreach ($countries as $c) {
+            $country = new Country();
+            $country->setName($c['name']);
+            $country->setCode($c['code']);
+            $country->setContinent($c['continent']);
+
+            $manager->persist($country);
+        }
+        $manager->flush();
     }
 }
